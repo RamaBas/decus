5ca3f33f91ff8d9ae8e60a0a3b3534b698ad282c
Rama Bastons <ramabastons@gmail.com>
2025-06-29 20:47:38 -0300

    Add cors to server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://<TUNOMBRE>.vercel.app']
+  origin: ['http://localhost:5173', 'https://decus-five.vercel.app/']
 }));
 app.use(express.json());
 
